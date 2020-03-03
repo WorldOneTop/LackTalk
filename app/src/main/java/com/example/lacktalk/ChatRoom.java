556137a597ff71d80d1fb75886a750948c8fe3d2
dlwpdlf147 <dlwpdlf147@naver.com>
2020-03-04 03:32:36 +0900

    채팅룸 서랍 UI및 클릭 이벤트확인만 완성

--- app/src/main/java/com/example/lacktalk/ChatRoom.java
+++ app/src/main/java/com/example/lacktalk/ChatRoom.java
@@ -17,7 +17,9 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,13 +32,15 @@ public class ChatRoom extends AppCompatActivity implements View.OnClickListener 
     Intent intent;
     Random random;
     ImageView imageView_back, imageView_search, imageView_menu, imageView_add, imageView_send;
-    TextView textView_name;
+    ImageView drawer_alarm,drawer_exit,drawer_setting;
+    TextView textView_name,drawer_main,drawer_picture,drawer_file;
     EditText editText_chat;
     AdapterChat adapter;
     InputMethodManager imm;
     DrawerLayout drawer;
     TableLayout under_Table;
-    ListView listView;
+    ListView listView,drawer_listview;
+    RelativeLayout drawer_rootLayout;
     Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +71,7 @@ public class ChatRoom extends AppCompatActivity implements View.OnClickListener 
     }
 
     public void init() {
+        //채팅방 위에 액션바(뒤로가기 이름 등등 ) 뷰 선언
         View include_layout = findViewById(R.id.include_actionbar);
         include_layout.findViewById(R.id.action_bar_chatRoom).setVisibility(View.VISIBLE);
         include_layout.findViewById(R.id.action_bar_friend).setVisibility(View.GONE);
@@ -75,6 +80,7 @@ public class ChatRoom extends AppCompatActivity implements View.OnClickListener 
         imageView_menu = include_layout.findViewById(R.id.icon_menu);
         textView_name = include_layout.findViewById(R.id.icon_name);
 
+        //채팅방 아래 ( edittext, +버튼 등등 ) 뷰 선언
         View include_underbar = findViewById(R.id.include_underbar);
         include_underbar.findViewById(R.id.underbar_Room).setVisibility(View.VISIBLE);
         include_underbar.findViewById(R.id.underbar_List).setVisibility(View.GONE);
@@ -83,22 +89,31 @@ public class ChatRoom extends AppCompatActivity implements View.OnClickListener 
         editText_chat = include_underbar.findViewById(R.id.edttext_chatroom);
         drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
 
+        //채팅방 서랍 열면 나오는 뷰 선언
+        drawer_alarm = findViewById(R.id.chatroom_drawer_chatAlarm);
+        drawer_exit = findViewById(R.id.chatroom_drawer_chatOut);
+        drawer_setting = findViewById(R.id.chatroom_drawer_chatSetting);
+        drawer_main = findViewById(R.id.chatroom_drawer_main);
+        drawer_picture = findViewById(R.id.chatroom_drawer_picture);
+        drawer_file = findViewById(R.id.chatroom_drawer_file);
+        drawer_listview = findViewById(R.id.chatroom_drawer_listview);
+        drawer_rootLayout = findViewById(R.id.drawerLayout_root);
+
+        //그외 기본 나와있는 뷰 및 변수 선언
         listView = findViewById(R.id.listView);
         under_Table = include_underbar.findViewById(R.id.underbar_Table);
-
-        under_Table.setMinimumHeight(Intro.HEIGHT_KEYBOARD);
-
         adapter = new AdapterChat();
-        listView.setAdapter(adapter);
-
         intent = getIntent();
-        textView_name.setText(intent.getExtras().getString("name"));
-
-        imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);//키보드 이벤트 발생용
-        imm.hideSoftInputFromWindow(editText_chat.getWindowToken(), 0);
-
         handler = new Handler();
+        imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);//키보드 이벤트 발생용
 
+
+        //디폴트 설정
+        drawer_alarm.setTag(1);//알람켜짐 태그
+        under_Table.setMinimumHeight(Intro.HEIGHT_KEYBOARD);//+버튼 누른 후 나오는 만큼을 키보드 높이만
+        listView.setAdapter(adapter);//리스트뷰 커스텀 어뎁터 설정
+        textView_name.setText(intent.getExtras().getString("name"));//채팅방 이름 설정
+        drawer_rootLayout.setOnClickListener(null);//서랍 뒤가 터치 안되게 막아놓음
         init_ClickListener();
     }
 
@@ -108,6 +123,15 @@ public class ChatRoom extends AppCompatActivity implements View.OnClickListener 
         imageView_menu.setOnClickListener(this);
         imageView_add.setOnClickListener(this);
         imageView_send.setOnClickListener(this);
+
+        //서랍 부분
+        drawer_main.setOnClickListener(this);
+        drawer_picture.setOnClickListener(this);
+        drawer_file.setOnClickListener(this);
+        drawer_exit.setOnClickListener(this);
+        drawer_alarm.setOnClickListener(this);
+        drawer_setting.setOnClickListener(this);
+
 
         //일단은 키보드 내리기용 이후 기능 추가(ex 답장 삭제)
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -174,6 +198,33 @@ public class ChatRoom extends AppCompatActivity implements View.OnClickListener 
                     editText_chat.setText("");
                     adapter.notifyDataSetChanged();
                 }
+                break;//이밑에는 서랍
+            case R.id.chatroom_drawer_main:
+                Toast.makeText(this,"공지사항 클릭, 스크롤뷰로 내역 뜨게 할것",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.chatroom_drawer_picture:
+                Toast.makeText(this,"사진동영상 클릭, 스크롤뷰로 내역 뜨게 할것",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.chatroom_drawer_file:
+                Toast.makeText(this,"파일 클릭, 스크롤뷰로 내역 뜨게 할것",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.chatroom_drawer_chatOut:
+                Toast.makeText(this,"나가기 클릭, 다이얼로그로 확인받고 나가게하기",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.chatroom_drawer_chatAlarm:
+                if((int)drawer_alarm.getTag() == 1) {
+                    Toast.makeText(this, "해당 방 알람이 해제 되었습니다.", Toast.LENGTH_SHORT).show();
+                    drawer_alarm.setImageResource(R.drawable.icon_no_alarm);
+                    drawer_alarm.setTag(0);
+                }
+                else{
+                    Toast.makeText(this, "해당 방 알람이 설정 되었습니다.", Toast.LENGTH_SHORT).show();
+                    drawer_alarm.setImageResource(R.drawable.icon_alarm);
+                    drawer_alarm.setTag(1);
+                }
+                break;
+            case R.id.chatroom_drawer_chatSetting:
+                Toast.makeText(this,"설정 클릭, 채팅방 설정 전용 액티비티로 이동",Toast.LENGTH_SHORT).show();
                 break;
         }
     }
